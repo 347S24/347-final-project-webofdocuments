20d50d1c2eda4aa206b0b51f9c20e856b0c20a10
Hudson Shaeffer <hudsonshaeffer@gmail.com>
2024-03-27 18:16:40 -0400

    added custom user model for future and ran migrations

--- wod/user/models.py
+++ wod/user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+"""Model represents a user of the webapp"""
+class CustomUser(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [email]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
