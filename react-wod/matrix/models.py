fce891813f35e927f012c951b0bcfcf753f08ba3
Kevin Chen <chen6kx@dukes.jmu.edu>
2024-04-10 16:37:49 -0400

    temp

--- react-wod/matrix/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-# from django.urls import reverse
-
-"""Model represents a collection of files"""
-class Matrix(models.Model):
-    # main data that will be stored in a matrix (a group of files)
-    documents = models.FileField()
-    
-    
