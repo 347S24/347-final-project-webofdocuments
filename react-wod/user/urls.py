1fc07c067725278b4697a4e7ee4bdaf898944d16
Hudson Shaeffer <hudsonshaeffer@gmail.com>
2024-04-11 11:19:37 -0400

    added user pages for login & logout / email & pass. reset

--- react-wod/user/urls.py
+++ react-wod/user/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
+    # adds all the auth paths
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
