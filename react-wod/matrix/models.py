51e5fa9b7a24631378766145e48f488a3b48ab9f
ethanhimes66 <ehimes3000@gmail.com>
2024-04-11 12:19:11 -0400

    Matrix model update

--- react-wod/matrix/models.py
+++ react-wod/matrix/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-# from django.urls import reverse
 
 """Model represents a collection of files"""
 class Matrix(models.Model):
     # main data that will be stored in a matrix (a group of files)
-    documents = models.FileField()
+    documents = models.CharField(max_length=100)
+
+
+class Unit(models.Model):
+    group = models.ForeignKey(Matrix, on_delete=models.CASCADE)
+    unit = models.FileField(upload_to='unit/')
     
-    
+    # Don't need to have but is helpful
+    created = models.DateTimeField(auto_now_add=True)
