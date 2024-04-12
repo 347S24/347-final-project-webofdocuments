331817ce362e687cfcaf23d4bb568cdf7797c5a6
Kevin Chen <chen6kx@dukes.jmu.edu>
2024-04-12 14:18:18 -0400

    don't need that directory and have a regex path for django to pass most things to react

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include
+from django.urls import include, re_path
 from django.urls import path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
@@ -12,7 +12,6 @@ from .views import HomeView
 
 urlpatterns = [
     # path("", TemplateView.as_view(template_name="index.html"), name="home"),
-    path("", HomeView.as_view(), name="home"),
     path(
         "about/",
         TemplateView.as_view(template_name="pages/about.html"),
@@ -24,7 +23,7 @@ urlpatterns = [
     path("users/", include("react_wod.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    # ...
+    re_path(".*", HomeView.as_view(), name="home"),
     # Media files
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
