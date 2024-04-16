0839ff1e64f10281b71ba1723acb2db28de11f71
Hudson Shaeffer <hudsonshaeffer@gmail.com>
2024-04-16 11:42:03 -0400

    did database stuff with stewart, i think its alright?

--- react_wod/users/models.py
+++ react_wod/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -9,12 +10,25 @@ class User(AbstractUser):
     Default custom user model for Ultimate react_wod.
     If adding fields that need to be filled at user signup,
     check forms.SignupForm and forms.SocialSignupForms accordingly.
+
+    Email and password are required. Other fields are optional.
     """
 
     # First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore[assignment]
     last_name = None  # type: ignore[assignment]
+
+    #TODO: is this allowed with django allauth??????????
+    # email = models.EmailField(_("email address"), unique=True)
+    # username = None # type: ignore[assignment]
+    # USERNAME_FIELD = "email"
+    # REQUIRED_FIELDS = [email]
+
+    
+    #TODO: custom manager required????
+    # objects = UserManager()
+
 
     def get_absolute_url(self) -> str:
         """Get URL for user's detail view.
