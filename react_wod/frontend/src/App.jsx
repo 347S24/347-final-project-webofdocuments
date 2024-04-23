5b7581bf4526cd3516e0b3d456a1b24d0ad09da9
Hudson Shaeffer <hudsonshaeffer@gmail.com>
2024-04-23 12:28:01 -0400

    side nav bar template made

--- react_wod/frontend/src/App.jsx
+++ react_wod/frontend/src/App.jsx
@@ -12,32 +12,28 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://www.djangoproject.com/" target="_blank">
-          <img src={djangoLogo} className="logo" alt="Django logo" />
-        </a>
+    <nav class="navbar">
+
+      <a href="/" class="home-btn">Home</a>
+
+      {
+        username ? <a href="/accounts/logout/" class="login-btn">Log Out</a> : <a href="/accounts/login/" class="login-btn">Log In</a>
+      }
+
+      <div class="create-btns">
+        <a href="" class='create-btn'>New Matrix</a>
+        <a href="" class='create-btn'>New Node</a>
       </div>
-      <h1>Vite + React + Django</h1>
+
+      <div class='matrix-name'>placeholder matrix</div>
+      <div class='list-nodes'>
+      </div>
+    </nav>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
         <p>
           You are logged in as {username}.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- react_wod/users/urls.py
+++ react_wod/users/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
-    path('accounts/', include('django.contrib.auth.urls')),
 
 ]
