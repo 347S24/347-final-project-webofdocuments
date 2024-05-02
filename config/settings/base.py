eecdeff886a920540c273470be7181266d1a5ebd
Hudson Shaeffer <hudsonshaeffer@gmail.com>
2024-05-02 12:14:50 -0400

    base.py update staticfiles

--- config/settings/base.py
+++ config/settings/base.py
@@ -156,8 +156,9 @@ STATIC_ROOT = str(BASE_DIR / "staticfiles")
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
-    str(APPS_DIR / "static"),
-    str(APPS_DIR / "templates" / "static"),
+    # str(APPS_DIR / "static"),
+    # str(APPS_DIR / "templates" / "static"),
+    str(BASE_DIR / "staticfiles")
 ]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
